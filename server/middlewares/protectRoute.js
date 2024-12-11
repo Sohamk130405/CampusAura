6169e4eef7af14e8e6b1af7000531edd813070e0
Soham Kolhatkar <sohamk130405@gmail.com>
2024-12-11 10:23:22 +0530

    bug fixes

--- server/middlewares/protectRoute.js
+++ server/middlewares/protectRoute.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken");
 
 module.exports.protectRoute = async (req, res, next) => {
   try {
-    const token = req.cookies.token;
+    const token = req.cookies.token || req.headers.authorization.split(" ")[1];
     if (!token) return res.status(401).json({ message: "Unauthorized!" });
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     const user = await User.findById(decoded.userId).select("-password");
